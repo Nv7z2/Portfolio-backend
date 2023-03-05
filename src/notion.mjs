ba01774bf8ddc29c2c23ce5a7012296fe86ddbe1
nv7z2 <barankacper2000@gmail.com>
2023-03-05 18:01:13 +0100

    feat(notion.js): add query filters

--- src/notion.mjs
+++ src/notion.mjs
@@ -5,8 +5,12 @@ dotenv.config();
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-export async function getDatabase(database) {
-  return await notion.databases.query({
+export async function getDatabase({ database, sort = null }) {
+  const queryObject = {
     database_id: database,
-  });
+  }
+
+  if (!!sort) queryObject.sorts = sort;
+
+  return await notion.databases.query(queryObject);
 }
